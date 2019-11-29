52f98b1b29863c336a554969ae67f7068a84770d
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-29 18:19:54 +0100

    :truck: Rename Header to About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
+import About from "./components/About";
 
 import "react-bulma-components/dist/react-bulma-components.min.css";
 import Navigation from "./components/Navigation";
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Navigation />
-      <Header />
+      <About />
     </div>
   );
 }
