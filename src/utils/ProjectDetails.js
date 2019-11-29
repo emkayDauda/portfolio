ce224bdecb2b5c1416a1429685714737943298b1
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-29 23:47:40 +0100

    :construction: Add dummy array of projects

--- src/utils/ProjectDetails.js
+++ src/utils/ProjectDetails.js
@@ -1,3 +1,22 @@
+import BnBalyze_Home from "../assets/BnBalyze_Home.png";
+
 export default  [
-    {}
+    {
+        projectTitle: `BnB Optimizer`,
+        projectImage: BnBalyze_Home,
+        stack: `HTML, CSS(LESS)`,
+        projectDescription: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia assumenda maiores asperiores laboriosam tempore temporibus impedit commodi hic illum velit? Aspernatur magnam praesentium repellat. At optio et cum. Culpa, dignissimos.`
+    },
+    {
+        projectTitle: `BnB Optimizer`,
+        projectImage: BnBalyze_Home,
+        stack: `HTML, CSS(LESS)`,
+        projectDescription: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia assumenda maiores asperiores laboriosam tempore temporibus impedit commodi hic illum velit? Aspernatur magnam praesentium repellat. At optio et cum. Culpa, dignissimos.`
+    },
+    {
+        projectTitle: `BnB Optimizer`,
+        projectImage: BnBalyze_Home,
+        stack: `HTML, CSS(LESS)`,
+        projectDescription: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia assumenda maiores asperiores laboriosam tempore temporibus impedit commodi hic illum velit? Aspernatur magnam praesentium repellat. At optio et cum. Culpa, dignissimos.`
+    }
 ]
