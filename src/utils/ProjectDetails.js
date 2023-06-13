ef458ea1ec5d896186f261df79eeb4629b0be7f3
Maaruf Dauda <mkdauda.work@gmail.com>
2023-06-13 18:56:48 +0100

    Remove old projects

--- src/utils/ProjectDetails.js
+++ src/utils/ProjectDetails.js
@@ -25,30 +25,30 @@ export default [
     gitHubLink: `#`,
     hostedSite: `#`
   },
-  {
-    title: `BnB Optimizer`,
-    image: [BnBalyze_Home],
-    stack: `HTML, CSS(LESS)`,
-    description: `BnB Optimizer is an app that uses past AirBnB datasets to determine the optimal pricing of an AirBnB unit based upon features such as geographic location, size, bedrooms, etc. On this project, I was responsible for the marketing (home) page and the 'About Us' page`,
-    gitHubLink: `https://github.com/emkayDauda`,
-    hostedSite: `https://happy-murdock-485e4d.netlify.com/`
-  },
-  {
-    title: `React Wars`,
-    image: ['https://res.cloudinary.com/emkaydauda/image/upload/c_fill,h_1536,w_1536/v1575478004/React_Wars.jpg'],
-    stack: `React, Styled Components`,
-    description: `Leveraging the Star Wars API for characters, React Wars depicts Star Wars characters and the attributes fans care about. This was built as a three-hour sprint challenge at Lambda School to demonstrate mastery of course content. `,
-    gitHubLink: `https://github.com/emkayDauda/Sprint-Challenge-React-Wars`,
-    hostedSite: `https://starwars-mk.netlify.com/`
-  },
-  {
-    title: `Weight Lifting Journal`,
-    image: ['https://res.cloudinary.com/emkaydauda/image/upload/v1575478039/spotter_journal.png'],
-    stack: `React, Redux, Node, Express, Bulma`,
-    description: `It's hard to keep track of how many reps you can do for time, or how much you usually deadlift. Weight Lifting Journal keeps it all organized and tracked for you. Mad gains from all your effort? Upload workout photos and brag. You've earned that much.`,
-    gitHubLink: `https://github.com/weight-lifting-journal-bw/weight-lifting-journal-react-app`,
-    hostedSite: `https://spotter-journal.netlify.com/`
-  },
+  // {
+  //   title: `BnB Optimizer`,
+  //   image: [BnBalyze_Home],
+  //   stack: `HTML, CSS(LESS)`,
+  //   description: `BnB Optimizer is an app that uses past AirBnB datasets to determine the optimal pricing of an AirBnB unit based upon features such as geographic location, size, bedrooms, etc. On this project, I was responsible for the marketing (home) page and the 'About Us' page`,
+  //   gitHubLink: `https://github.com/emkayDauda`,
+  //   hostedSite: `https://happy-murdock-485e4d.netlify.com/`
+  // },
+  // {
+  //   title: `React Wars`,
+  //   image: ['https://res.cloudinary.com/emkaydauda/image/upload/c_fill,h_1536,w_1536/v1575478004/React_Wars.jpg'],
+  //   stack: `React, Styled Components`,
+  //   description: `Leveraging the Star Wars API for characters, React Wars depicts Star Wars characters and the attributes fans care about. This was built as a three-hour sprint challenge at Lambda School to demonstrate mastery of course content. `,
+  //   gitHubLink: `https://github.com/emkayDauda/Sprint-Challenge-React-Wars`,
+  //   hostedSite: `https://starwars-mk.netlify.com/`
+  // },
+  // {
+  //   title: `Weight Lifting Journal`,
+  //   image: ['https://res.cloudinary.com/emkaydauda/image/upload/v1575478039/spotter_journal.png'],
+  //   stack: `React, Redux, Node, Express, Bulma`,
+  //   description: `It's hard to keep track of how many reps you can do for time, or how much you usually deadlift. Weight Lifting Journal keeps it all organized and tracked for you. Mad gains from all your effort? Upload workout photos and brag. You've earned that much.`,
+  //   gitHubLink: `https://github.com/weight-lifting-journal-bw/weight-lifting-journal-react-app`,
+  //   hostedSite: `https://spotter-journal.netlify.com/`
+  // },
   {
     title: `mPOS`,
     image: ['https://res.cloudinary.com/emkaydauda/image/upload/bo_1px_solid_rgb:000000,c_scale,h_1280,q_100,r_0,w_720/bo_3px_solid_rgb:000000,c_fill,h_1280,l_mpos_home,r_0,w_720,x_720,y_0/v1575479030/mpos.jpg'],
